49245094d58fc9df7a1d2567ad4cd816845d99b3
vuun <sown.squak@gmail.com>
2017-08-24 17:24:17 +0700

    Project added

---
+++ Screenshot/Assets/ImageViewer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+public class ImageViewer : MonoBehaviour {
+
+    string filename;
+
+    void Start()
+    {
+        var directory = new DirectoryInfo("C:\\Users\\vuun\\Desktop");
+        var load_filename = directory.GetFiles("*screen_2048x2048*.png*", SearchOption.AllDirectories).OrderByDescending(f => f.LastWriteTime).First();
+        Debug.Log(load_filename);
+        filename = load_filename.ToString();
+    }
+    void OnGUI()
+    {
+        if (filename != null)
+        {
+            Texture2D LTexture = LoadPNG(filename);
+            GUI.DrawTexture(new Rect(0f, 0f, Screen.width, Screen.height), LTexture);
+        }
+    }
+
+    public static Texture2D LoadPNG(string filePath)
+    {
+        
+        Texture2D tex = null;
+        byte[] fileData;
+
+        if (File.Exists(filePath))
+        {
+            fileData = File.ReadAllBytes(filePath);
+            tex = new Texture2D(2, 2);
+            tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+        }
+        return tex;
+    }
+}
---
+++ Screenshot/Assets/PathInput.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PathInput : MonoBehaviour {
+
+    public InputField input;
+    public string path = " ";
+
+    // Use this for initialization
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            input.gameObject.SetActive(true);
+        }
+
+    }
+    public void get()
+    {
+        path = input.text;
+        input.gameObject.SetActive(false);
+    }
+
+}
